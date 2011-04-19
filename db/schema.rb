4c5f7a54ae072b3bfaca88ec2d7e50475127133c
Mark Lopez <marklopez@devml03.home>
2011-04-19 07:29:17 -0400

    small change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110404093043) do
+ActiveRecord::Schema.define(:version => 20110407011436) do
 
   create_table "deposits", :force => true do |t|
     t.integer  "user_id"
