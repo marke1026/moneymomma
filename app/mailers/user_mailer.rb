3a7bf67f5f7efcd84874279b1424a264a078e81a
gaurav singh <gauravsingh@static-177.254.201.49.tataidc.co.in>
2011-04-20 10:18:53 +0530

    testing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,13 @@
 class UserMailer < ActionMailer::Base
-  default :from => "from@example.com"
-  default_url_options[:host] = "localhost:3000"
+  if Rails.env == 'development'
+    default :from => "from@example.com"
+    default_url_options[:host] = "localhost:3000"
+  end
+  
+  if Rails.env == 'production'
+    default :from => "mymoneymomma.heroku.com"
+    default_url_options[:host] = "mymoneymomma.heroku.com"
+  end
   
   def activation_instructions(user)
     @account_activation_url = activate_url(user.perishable_token)
