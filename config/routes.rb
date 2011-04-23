e30b00df06a9dbf5329c48e057a2d9038e16f6ff
Mark Lopez <marklopez@devml03.home>
2011-04-23 07:55:01 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ Money::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "payments#home"
+  root :to => "payments#index"
 
   # See how all your routes lay out with "rake routes"
 
