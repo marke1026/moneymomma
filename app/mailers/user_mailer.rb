fbae175ed5c1690721ca1342475810b7c3e481bb
Mark Lopez <marklopez@devml03.home>
2011-04-24 00:11:40 -0400

    changed config settings

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,17 +6,17 @@ class UserMailer < ActionMailer::Base
   
   if Rails.env == 'production'
     default :from => "mymoneymomma.heroku.com"
-    default_url_options[:host] = "mymoneymomma.heroku.com"
+    default_url_options[:host] = "mymoneymomma.com"
   end
   
   def activation_instructions(user)
     @account_activation_url = activate_url(user.perishable_token)
-    mail(:to => user.email, :subject => "Activation Instructions") 
+    mail(:to => user.email, :subject => "Money Momma Activation Instructions") 
     content_type "text/html"
   end
   
   def welcome(user)
-    mail(:to => user.email, :subject => "Welcome to the Site!")
+    mail(:to => user.email, :subject => "Welcome to Money Momma. You are on your way to take command of your finances forever!")
     @root_url = root_url
     content_type "text/html"
   end
@@ -24,6 +24,6 @@ class UserMailer < ActionMailer::Base
   def password_reset_instructions(user)   
     @edit_password_reset_url = edit_password_reset_url(user.perishable_token)
     content_type "text/html"
-    mail(:to => user.email, :subject => "Password Reset Instructions")
+    mail(:to => user.email, :subject => "Money Momma Password Reset Instructions")
   end
 end
