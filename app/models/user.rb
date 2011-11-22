d9074c601b531b1eb7b4131635222886c5b5adfb
Mark Lopez <marklopez@new-host-2.home>
2011-11-22 06:33:48 -0500

    Comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -94,8 +94,8 @@ class User < ActiveRecord::Base
   end
   
   def send_email_alerts_before_paycheck
-    dates_before_payments = self.payments.all.map{|p| p.transaction_dates}.flatten.map{|b| b-9}
-    dates_before_deposits = self.deposits.all.map{|d| d.deposit_dates}.flatten.map{|c| c-9}
+    dates_before_payments = self.payments.all.map{|p| p.transaction_dates}.flatten.map{|b| b-8}
+    dates_before_deposits = self.deposits.all.map{|d| d.deposit_dates}.flatten.map{|c| c-8}
     if dates_before_payments.include?(Date.today) || dates_before_deposits.include?(Date.today)
       UserMailer.alert_before_paycheck(self).deliver
     else
