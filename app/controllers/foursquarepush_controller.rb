16f91aa8a6bde166642335720aa3ea07c3470ed7
Mark Lopez <mlopez@marqeta.com>
2012-06-26 18:22:57 -0400

    commit my changes

--- app/controllers/foursquarepush_controller.rb
+++ app/controllers/foursquarepush_controller.rb
@@ -5,6 +5,8 @@ class FoursquarepushController < ApplicationController
   def notify
     sms = Moonshado::Sms.new("18139577566", "This is a test Foursquare checkin notifcation")
     sms.deliver_sms
+    sms = Moonshado::Sms.new("17278715066", "This is a test Foursquare checkin notifcation")
+    sms.deliver_sms
     FoursquareHandler.save_from_push(params)
     render :nothing => true
   end
