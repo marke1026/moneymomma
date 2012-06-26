cb68bf067c31891c5bb3ed7b16d4658258b60cf2
Matthew Solt <mattsolt@gmail.com>
2012-06-26 00:09:00 -0700

    fix authenticity token on foursquarepush

--- app/controllers/foursquarepush_controller.rb
+++ app/controllers/foursquarepush_controller.rb
@@ -1,5 +1,7 @@
 class FoursquarepushController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
+
   def notify
     FoursquareHandler.save_from_push(params)
     render :nothing => true
