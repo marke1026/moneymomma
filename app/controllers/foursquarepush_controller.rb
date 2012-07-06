1c4fbc1c337a6787c9090abcb9517daaac0da6a8
Mark Lopez <mlopez@marqeta.com>
2012-07-06 11:27:49 -0400

    addded shit

--- app/controllers/foursquarepush_controller.rb
+++ app/controllers/foursquarepush_controller.rb
@@ -5,10 +5,11 @@ class FoursquarepushController < ApplicationController
   def notify
     
     user = params['user']
-    fn = user['firstName']
-    sms = Moonshado::Sms.new("18139577566", "Bam! " + fn + " " + user['lastName'] + " just checkin on Foursquare!")
+   
+    :first_name => user['firstName'],
+    sms = Moonshado::Sms.new("18139577566", "Bam! " + :first_name + " " + user['lastName'] + " just checkin on Foursquare!")
     sms.deliver_sms
-    sms = Moonshado::Sms.new("17278715066", "Bam! " + fn + " "  + user['lastName'] + " just checkin on Foursquare!")
+    sms = Moonshado::Sms.new("17278715066", "Bam! " + :first_name + " "  + user['lastName'] + " just checkin on Foursquare!")
     sms.deliver_sms
     FoursquareHandler.save_from_push(params)
     render :nothing => true
