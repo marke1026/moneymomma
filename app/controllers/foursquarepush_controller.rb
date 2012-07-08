c2fdce29f75d9adbd76ac9aa0433c08480d55ae4
Kaleem Ullah <kaleem.bahrian@gmail.com>
2012-07-08 22:04:11 +0400

    inspect foursquare push params

--- app/controllers/foursquarepush_controller.rb
+++ app/controllers/foursquarepush_controller.rb
@@ -5,9 +5,14 @@ class FoursquarepushController < ApplicationController
   def notify
     
     user = params['user']
-   debugger
+    puts ">>>>User<<<<<"
+    puts user.inspect
+    puts ">>>>Params"
+    puts params.inspect
     fp =  user['firstName'].to_s
+    
     puts user['firstName'].to_s
+
     sms = Moonshado::Sms.new("18139577566", "Bam!" + fp.to_s + "  just checkin on Foursquare!")
     sms.deliver_sms
     sms = Moonshado::Sms.new("17278715066", "Bam! "  + fp + "just checkin on Foursquare!")
