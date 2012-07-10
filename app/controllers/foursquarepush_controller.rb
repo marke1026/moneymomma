c35206d57b136bdac5591e6105aaa8a2e666fca3
Mark Lopez <mlopez@marqeta.com>
2012-07-10 17:09:01 -0400

    fixed

--- app/controllers/foursquarepush_controller.rb
+++ app/controllers/foursquarepush_controller.rb
@@ -5,17 +5,21 @@ class FoursquarepushController < ApplicationController
   def notify
     
    user = JSON.parse(params['user'])
+   checkin = JSON.parse(params['checkin'])
 
+
+     venue = checkin['venue']
+     puts venue
    #puts  v
     #user = params['user']
     #fn =  user['firstName'].to_s
     #ln = user['lastName'].to_s
 
 
-    #sms = Moonshado::Sms.new("18139577566", "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + name.to_s)
-    #sms.deliver_sms
-    #sms = Moonshado::Sms.new("17278715066",  "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + name.to_s)
-    #sms.deliver_sms
+    sms = Moonshado::Sms.new("18139577566", "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + venue.to_s)
+    sms.deliver_sms
+    sms = Moonshado::Sms.new("17278715066",  "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + venue.to_s)
+    sms.deliver_sms
     FoursquareHandler.save_from_push(params)
 
     puts FoursquareVenue.last.inspect
