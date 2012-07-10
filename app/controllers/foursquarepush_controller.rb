c155947b4bf6a4fe3d754a870b36502c799c137c
Mark Lopez <mlopez@marqeta.com>
2012-07-10 17:12:42 -0400

    fixed

--- app/controllers/foursquarepush_controller.rb
+++ app/controllers/foursquarepush_controller.rb
@@ -14,11 +14,12 @@ class FoursquarepushController < ApplicationController
     #user = params['user']
     fn =  user['firstName'].to_s
     ln = user['lastName'].to_s
+    name = venue['name']
 
 
-    sms = Moonshado::Sms.new("18139577566", "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + venue.to_s)
+    sms = Moonshado::Sms.new("18139577566", "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + name.to_s)
     sms.deliver_sms
-    sms = Moonshado::Sms.new("17278715066",  "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + venue.to_s)
+    sms = Moonshado::Sms.new("17278715066",  "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + name.to_s)
     sms.deliver_sms
     FoursquareHandler.save_from_push(params)
 
