16d39729b5bdb73794a5bb66ca207c47391da94f
Kaleem Ullah <kaleem.bahrian@gmail.com>
2012-07-10 05:33:21 +0400

    inspec checkin params

--- app/controllers/foursquarepush_controller.rb
+++ app/controllers/foursquarepush_controller.rb
@@ -5,8 +5,9 @@ class FoursquarepushController < ApplicationController
   def notify
     
    user = JSON.parse(params['user'])
-   puts params.inspect
-   venue = JSON.parse(params['venue'])
+
+   checkin = JSON.parse(params['checkin'])
+   venue = params['checkin']
    puts ">>>>>>>Venue<<<<<<<<<<<<"
    puts venue.inspect
    puts venue['name']
@@ -21,7 +22,14 @@ class FoursquarepushController < ApplicationController
   #  name = venue['name'].to_s
    #name  = 'test' 
     
-   
+   {"checkin"=>"{\"id\":\"4ffb83b2e4b02c1b10e52019\",\"createdAt\":1341883314,\"type\":\"checkin\",\"timeZone\":\"Asia\\/Dubai\",\"timeZoneOffset\":240,
+    \"user\":{\"id\":\"10779936\",\"firstName\":\"Kaleem\",\"lastName\":\"Ullah\",\"relationship\":\"self\",\"photo\":\"https:\\/\\/is0.4sqi.net\\/userpix_thumbs\\/0E5N5MVVNKPSDBMM.jpg\",
+    \"tips\":{\"count\":0},
+    \"lists\":{\"groups\":[{\"type\":\"created\",\"count\":1,\"items\":[]}]},\"gender\":\"male\",\"homeCity\":\"Abu Dhabi, United Arab Emirates\",\"bio\":\"\",
+    \"contact\":{\"email\":\"kaleembinkhan@gmail.com\",\"twitter\":\"kaleemullah\",\"facebook\":\"722104282\"}},
+    \"venue\":{\"id\":\"4e1cfb791838f4ab743a96f4\",\"name\":\"Delma Street, Abu Dhabi\",\"contact\":{},
+    \"location\":{\"lat\":24.464478109343794,\"lng\":54.37952937910345,\"city\":\"Abu Dhabi\",\"state\":\"Abu Dhabi\",\"country\":\"United Arab Emirates\",\"cc\":\"AE\"},\"categories\":[],\"verified\":false,\"stats\":{\"checkinsCount\":335,\"usersCount\":48,\"tipCount\":1},\"likes\":{\"count\":0,\"groups\":[]},\"beenHere\":{\"count\":0}}}", "user"=>"{\"id\":\"10779936\",\"firstName\":\"Kaleem\",\"lastName\":\"Ullah\",\"relationship\":\"self\",\"photo\":\"https:\\/\\/is0.4sqi.net\\/userpix_thumbs\\/0E5N5MVVNKPSDBMM.jpg\",\"tips\":{\"count\":0},\"lists\":{\"groups\":[{\"type\":\"created\",\"count\":1,\"items\":[]}]},\"gender\":\"male\",\"homeCity\":\"Abu Dhabi, United Arab Emirates\",\"bio\":\"\",\"contact\":{\"email\":\"kaleembinkhan@gmail.com\",\"twitter\":\"kaleemullah\",\"facebook\":\"722104282\"}}", "secret"=>"UYLATHW4ZW0RLUYMNTIVBZS3L0JL003WJ3HOCP0VRX1B3MJX", "action"=>"notify", "controller"=>"foursquarepush"}
+
 
     #sms = Moonshado::Sms.new("18139577566", "Bam! " + fn.to_s + " " + ln.to_s + "  just checked in at " + name.to_s)
     #sms.deliver_sms
